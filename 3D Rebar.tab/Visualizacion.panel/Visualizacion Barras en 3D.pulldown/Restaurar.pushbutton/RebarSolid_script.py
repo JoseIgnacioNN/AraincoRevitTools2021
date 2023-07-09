09fdb2a5088685e4c19f15ed71ff11c098e59357
José Ignacio Núñez Navarrete <61503887+JoseIgnacioNN@users.noreply.github.com>
2023-07-09 18:37:10 -0400

    Up

---
+++ 3D Rebar.tab/Visualizacion.panel/Visualizacion Barras en 3D.pulldown/Restaurar.pushbutton/RebarSolid_script.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Importar los módulos necesarios
+import clr
+clr.AddReference("RevitAPI")
+clr.AddReference("RevitAPIUI")
+from Autodesk.Revit.DB import BuiltInCategory, FilteredElementCollector, Transaction
+
+# Obtener los servicios de Revit
+from pyrevit import revit, DB
+from pyrevit import script
+
+# Obtener el documento activo y la vista activa
+doc = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
+view = uidoc.ActiveView
+
+# Iniciar una transacción
+with revit.Transaction("Set Rebar Solid and Unobscured In View"):
+    # Recopilar todos los elementos de refuerzo
+    rebars = (
+        FilteredElementCollector(doc)
+        .OfCategory(BuiltInCategory.OST_Rebar)
+        .WhereElementIsNotElementType()
+        .ToElements()
+    )
+
+    # Iterar sobre cada elemento de refuerzo
+    for rebar in rebars:
+        # Establecer el refuerzo como sólido y no oculto en la vista
+        rebar.SetSolidInView(view, False)
+        rebar.SetUnobscuredInView(view, False)
